d922aec3ff18db3c05d4235d3c4ae5e38c6adfc4
Nirupama Hanumanthe Gowda <ngowda@v2soft.in>
2017-05-23 16:35:29 +0530

    init repo

---
+++ src/testing/Test.java
@@ -0,0 +1,17 @@
+package testing;
+
+import java.io.IOException;
+
+public class Test {
+	
+	public static void main (String[] args) throws IOException
+    {
+        
+  
+		//Runtime.getRuntime().exec("C:\\Users\\ngowda\\Desktop\\putty.exe");
+        //Runtime.getRuntime().exec("C:\\Users\\ngowda\\Desktop\\putty1.exe");
+		Runtime.getRuntime().exec("cmd /c start C://Users//ngowda//Desktop//ba.bat");
+        
+    }
+
+}
